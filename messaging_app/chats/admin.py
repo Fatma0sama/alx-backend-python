f46ecb73132fec4d2afdd7fbfb95b5ae30b2087f
Fatma0sama <165494703+Fatma0sama@users.noreply.github.com>
2025-11-16 21:39:12 +0200

    Add admin registration for messaging app models

---
+++ messaging_app/chats/admin.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Admin registration for messaging app"""
+from django.contrib import admin
+from .models import User, Conversation, Message
+
+admin.site.register(User)
+admin.site.register(Conversation)
+admin.site.register(Message)
