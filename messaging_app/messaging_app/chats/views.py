65c598aaa4814e059951243504f19b136f589cee
Fatma0sama <165494703+Fatma0sama@users.noreply.github.com>
2025-11-16 21:25:57 +0200

    Add views for Conversation and Message models

---
+++ messaging_app/messaging_app/chats/views.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Views for messaging_app"""
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
