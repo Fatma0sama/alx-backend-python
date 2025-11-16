74265d1009ada5a65b5100ec14d41102eb253e57
Fatma0sama <165494703+Fatma0sama@users.noreply.github.com>
2025-11-16 22:21:32 +0200

    Update utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,30 +1,23 @@
 #!/usr/bin/env python3
-"""Stub client.py for GithubOrgClient"""
-from typing import Any, List
-import utils  # make sure utils.py exists
+"""Stub utils.py with memoize, access_nested_map, get_json"""
+from typing import Any, Dict, Tuple
 
-class GithubOrgClient:
-    ORG_URL = "https://api.github.com/orgs/{org}"
+def access_nested_map(nested_map: Dict, path: Tuple[str, ...]) -> Any:
+    for key in path:
+        nested_map = nested_map[key]
+    return nested_map
 
-    def __init__(self, org: str) -> None:
-        self.org_name = org
+def get_json(url: str) -> dict:
+    """Stub that should be mocked in tests"""
+    return {}
+
+def memoize(fn):
+    """Simple memoize decorator"""
+    attr_name = "_memoized_" + fn.__name__
 
     @property
-    def org(self) -> dict:
-        """Return mocked org payload"""
-        return utils.get_json(self.ORG_URL.format(org=self.org_name))
-
-    @property
-    def _public_repos_url(self) -> str:
-        return self.org.get("repos_url", "")
-
-    def public_repos(self, license: str = None) -> List[str]:
-        repos = utils.get_json(self._public_repos_url)
-        names = [r["name"] for r in repos]
-        if license:
-            names = [r["name"] for r in repos if r.get("license", {}).get("key") == license]
-        return names
-
-    @staticmethod
-    def has_license(repo: dict, license_key: str) -> bool:
-        return repo.get("license", {}).get("key") == license_key
+    def wrapper(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+    return wrapper
