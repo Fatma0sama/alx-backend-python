62c8efc19cde86b42cf7f80ec9278b6cc495e95a
Fatma0sama <165494703+Fatma0sama@users.noreply.github.com>
2025-11-16 21:41:32 +0200

    Remove Conversation and Message models
    Removed Conversation and Message models from the user model file.

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -8,6 +8,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """Custom user model"""
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=150, null=False)
+    last_name = models.CharField(max_length=150, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=128, null=False)
     phone_number = models.CharField(max_length=20, null=True)
     role_choices = [
         ('guest', 'Guest'),
@@ -15,19 +19,3 @@ class User(AbstractUser):
         ('admin', 'Admin')
     ]
     role = models.CharField(max_length=10, choices=role_choices, default='guest')
-
-
-class Conversation(models.Model):
-    """Conversation between users"""
-    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    participants = models.ManyToManyField(User)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
-class Message(models.Model):
-    """Message sent in a conversation"""
-    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
-    message_body = models.TextField()
-    sent_at = models.DateTimeField(auto_now_add=True)
