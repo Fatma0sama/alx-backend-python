e2996ce0e536cfaf1c33b51ccf467886c299a45d
Fatma0sama <165494703+Fatma0sama@users.noreply.github.com>
2025-11-16 21:42:25 +0200

    Add Conversation and Message models to chats app

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -19,3 +19,19 @@ class User(AbstractUser):
         ('admin', 'Admin')
     ]
     role = models.CharField(max_length=10, choices=role_choices, default='guest')
+
+
+class Conversation(models.Model):
+    """Conversation model that tracks which users are involved"""
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    """Message model"""
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
