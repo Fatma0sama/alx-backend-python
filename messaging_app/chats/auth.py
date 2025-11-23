9c477a09707c99875026dc6c52832090740ec485
Fatma0sama <165494703+Fatma0sama@users.noreply.github.com>
2025-11-23 22:47:25 +0200

    Create auth.py

---
+++ messaging_app/chats/auth.py
@@ -0,0 +1,7 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+class CustomJWTAuthentication(JWTAuthentication):
+    """
+    Custom authentication class (required by project structure)
+    """
+    pass
