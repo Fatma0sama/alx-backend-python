a23e56395c666c4895fd21ad1ac95c25b226cddf
Fatma0sama <165494703+Fatma0sama@users.noreply.github.com>
2025-11-23 21:01:17 +0200

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -2,8 +2,14 @@
 """Main URLs for messaging_app"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # new
     path('api-auth/', include('rest_framework.urls')),  # required by the checker
