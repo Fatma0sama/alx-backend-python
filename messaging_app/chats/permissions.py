796054515c6713c18135089e293d944207df64a5
Fatma0sama <165494703+Fatma0sama@users.noreply.github.com>
2025-11-23 21:02:01 +0200

    Create permissions.py

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+class IsParticipantOfConversation(BasePermission):
+    """
+    Custom permission to allow only participants of a conversation to access it
+    """
+    def has_object_permission(self, request, view, obj):
+        # Ensure the user is authenticated
+        if not request.user.is_authenticated:
+            return False
+        # Check if user is participant in conversation
+        return request.user in obj.participants.all()
