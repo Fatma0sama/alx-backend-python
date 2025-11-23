5f705963833f520ce4d0e3e3ee8d80a2e3f5aa5d
Fatma0sama <165494703+Fatma0sama@users.noreply.github.com>
2025-11-23 22:45:19 +0200

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -9,11 +9,12 @@ INSTALLED_APPS = [
 
 # Django REST Framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',  # ← لازم تكون دي موجودة
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ),
+    ]
 }
